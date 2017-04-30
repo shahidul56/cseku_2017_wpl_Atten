e9d3701e502263c2f570b3e3903856dffe1489d5
MD. SHAHIDUL ISLAM <shahidul7889@gmail.com>
2017-04-30 11:02:10 +0600

    WEB Site Part
    the student will able to view attendance in online

---
+++ attn/Index.php
@@ -0,0 +1,49 @@
+<?php
+include 'excel_reader.php';     // include the class
+
+// creates an object instance of the class, and read the excel file data
+$excel = new PhpExcelReader;
+$excel->read('KHULNA-UNIVERSITY-CLASS-ATTENDANCE.xls');
+
+
+
+// this function creates and returns a HTML table with excel rows and columns data
+// Parameter - array with excel worksheet data
+function sheetData($sheet) {
+  $re = '<table>';     // starts html table
+
+  $x = 1;
+  while($x <= $sheet['numRows']) {
+    $re .= "<tr>\n";
+    $y = 1;
+    while($y <= $sheet['numCols']) {
+      $cell = isset($sheet['cells'][$x][$y]) ? $sheet['cells'][$x][$y] : '';
+      $re .= " <td>$cell</td>\n";  
+      $y++;
+    }  
+    $re .= "</tr>\n";
+    $x++;
+  }
+
+  return $re .'</table>';     // ends and returns the html table
+}
+
+
+
+
+$nr_sheets = count($excel->sheets);       // gets the number of worksheets
+$excel_data = '';              // to store the the html tables with data of each sheet
+
+
+
+
+
+// traverses the number of sheets and sets html table with each sheet data in $excel_data
+for($i=0; $i<$nr_sheets; $i++) {
+  $excel_data .= '<h4>Sheet '. ($i + 1) .' (<em>'. $excel->boundsheets[$i]['name'] .'</em>)</h4>'. sheetData($excel->sheets[$i]) .'<br/>';  
+}
+
+
+
+
+echo $excel_data;      // outputs HTML tables with excel file data
